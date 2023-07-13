aa2e9b4fa7e00a9845fa12a2d61f1bf0aa83acee
Дмитрий Истомин <vhg860@gmail.com>
2023-07-13 16:19:44 +0300

    1

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-    
+
     class Meta:
         model = Post
         exclude = ('author',)
@@ -17,7 +17,7 @@ class PostForm(forms.ModelForm):
 
 
 class CommentForm(forms.ModelForm):
-    
+
     class Meta:
         model = Comment
         fields = ('text',)
